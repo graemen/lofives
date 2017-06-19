fd9b2036efc52cf321c2000598ab0a6de36f7a98
graeme <graeme@lolux.net>
2017-06-19 12:17:25 +1200

    Update gloves.py

--- gloves.py
+++ gloves.py
@@ -1,3 +1,27 @@
+# ----------------------------------------------------------------------------------------------
+#   This file is part of LoFives - osc glove instrument software.
+#   This code runs on the C.H.I.P platform with an LSM9DS1 attached
+#
+#   LoFives is free software: you can redistribute it and/or modify it under the terms
+#   of the GNU General Public License as published by the Free Software Foundation, either
+#   version 3 of the license, or any later version.
+#
+#   LoFives is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+#   without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#   See the GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License along with
+#   LoFives.  If not, see http://www.gnu.org/licenses/
+#
+#
+#   Copyright 2017 LOlux productions (www.lolux.net)
+#
+#   Developed by :
+#
+#       graeme@lolux.net
+#
+# ----------------------------------------------------------------------------------------------
+
 import math
 import sys
 import time
